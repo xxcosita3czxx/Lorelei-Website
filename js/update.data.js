fd6d5c0d2233748ae9d78424b7c1ee16b8150d82
xxcosita3czxx <kokosove18@gmail.com>
2025-08-01 17:56:06 +0200

    test

--- js/update.data.js
+++ js/update.data.js
@@ -3,9 +3,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchInitialUptime('http://localhost:9921/uptime');
     if (document.getElementById('ping-info')) {
-        fetchData('http://localhost:9921/ping', 'ping');
+        fetchData('/ping', 'ping');
     }
     if (document.getElementById('guilds-info')) {
-        fetchData('http://localhost:9921/guilds', 'guilds');
+        fetchData('/guilds', 'guilds');
     }
 });
